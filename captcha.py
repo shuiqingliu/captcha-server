e0155cf1d7ecfe141dbe52a17269165f2d066ce6
shuiqingliu <shuiqingliu14@gmail.com>
2018-06-12 14:08:57 +0800

    finish img upload

--- captcha.py
+++ captcha.py
@@ -1,4 +1,7 @@
-from flask import (Flask,Blueprint, g,redirect,render_template,request,url_for)
+from flask import (Flask,Blueprint, g,redirect,render_template,request,url_for,jsonify)
+import json,os
+import requests
+from config import BASEDIR,GLOBAL
 
 bp = Blueprint('captcha', __name__, url_prefix='/captcha')
 
@@ -12,3 +15,36 @@ def test():
         pass
 
     return render_template('captcha/test.html')
+
+@bp.route('/api/upload', methods=['POST'])
+def upload():
+    response = {}
+    if request.method == 'POST':
+        img = request.files.get("file")
+        path = BASEDIR + "/static/imgs/"
+        file_path = path
+        filenameArray = img.filename.split('.')
+
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        if not os.path.isfile(file_path+img.filename):
+            img.save(file_path + img.filename)
+        else:
+            img.save(file_path + filenameArray[0].strip() + "_." + filenameArray[1])
+        print("uploaded image:" + img.filename)
+        response['status'] = 200
+        response['message'] = 'success'
+        return jsonify(response)
+    else:
+        response['status'] = 405
+        response['message'] = "不支持的方法"
+        return jsonify(response)
+
+# def imgrec(path):
+#     #read img
+#     #TODO：
+#
+#     url = GLOBAL["Server"]
+#     resonse = requests.post(url)
+#     result = json.loads(resonse.text)
--- config.py
+++ config.py
@@ -2,7 +2,9 @@ DATABASE = {
     "WeChatCollection" : "weixin_identycode"
 }
 
-GLOBAL = {
+BASEDIR = "/home/qingliu/captcha/"
 
+GLOBAL = {
+    "Server": 'http://172.30.157.96:5000/captcha?str=',
     "Port": 5004
 }
